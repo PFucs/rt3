6d877c8d8179365165c444e0c5c1c0fa7f8007c1
PFucs <fucskar.patricia@gmail.com>
2021-03-29 17:00:32 +0200

    added unit tests for TrainSensorImpl

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -1,5 +1,8 @@
 package hu.bme.mit.train.sensor;
 
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainSensor;
+import hu.bme.mit.train.interfaces.TrainUser;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -7,13 +10,42 @@ import static org.mockito.Mockito.*;
 
 public class TrainSensorTest {
 
+    private TrainSensorImpl ts;
+    private TrainController tc;
+    private TrainUser tu;
+
     @Before
     public void before() {
-        // TODO Add initializations
+        tc = mock(TrainController.class);
+        tu = mock(TrainUser.class);
+        ts= new TrainSensorImpl(tc, tu);
     }
 
     @Test
-    public void ThisIsAnExampleTestStub() {
-        // TODO Delete this and add test cases based on the issues
+    public void SpeedChangedTest() {
+        Assert.assertEquals(ts.getSpeedLimit(), 5);
+        ts.overrideSpeedLimit(7);
+        Assert.assertEquals(ts.getSpeedLimit(), 7);
+    }
+
+    @Test
+    public void SpeedSetTest(){
+        ts.overrideSpeedLimit(100);
+        verify(tc).setSpeedLimit(100);
+        verify(tu, never()).setAlarmState(true);
+    }
+
+    @Test
+    public void InvalidSpeedTest(){
+        ts.overrideSpeedLimit(-3);
+        verify(tc, never()).setSpeedLimit(-3);
+        verify(tu).setAlarmState(true);
+    }
+
+    @Test
+    public void LimitTest(){
+        ts.overrideSpeedLimit(500);
+        verify(tc).setSpeedLimit(500);
+        verify(tu, never()).setAlarmState(true);
     }
 }
